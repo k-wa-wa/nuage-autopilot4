b17d2a4f5a3c801bf6569b14a6b3641d26487ba8
Kohei Watanabe <sek.ohei.w0822@icloud.com>
2026-09-23 02:13:59 +0900

    change: font

--- src/view/document.tsx
+++ src/view/document.tsx
@@ -22,6 +22,12 @@ export function renderDocument(data: PageData, options: { extraStyles?: string }
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <title>{title}</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600;700&family=IBM+Plex+Sans+JP:wght@400;500;600;700&family=IBM+Plex+Mono:wght@400;500&display=swap"
+        />
         <style dangerouslySetInnerHTML={{ __html: styles + (options.extraStyles ?? "") }} />
       </head>
       <body>
